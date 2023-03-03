9b906f6ad64d842ac189bad513cce8ad1c3f9196
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-03-03 14:44:31 +0200

    Add scss support

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import './style.scss';
 // import App from './App.svelte'
 
 // const app = new App({
@@ -6,9 +6,3 @@
 // })
 
 // export default app
-
-console.log('hello');
-import hello from './hello.js';
-hello.hello('esmodule');
-let hello2 = require('./hello2.js');
-hello2.hello('commonjs');
