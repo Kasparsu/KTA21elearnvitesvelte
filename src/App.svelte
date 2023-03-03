c28f39ad2d68fa4997be4ada7e4bc5aaa153b596
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-03-03 15:21:38 +0200

    add bulma and svelte styles example

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<button class="button is-primary">Click me</button>
+
+<style lang="scss">
+  $color: red;
+  .button {
+    color: $color !important;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './style.scss';
-// import App from './App.svelte'
+import App from './App.svelte';
 
-// const app = new App({
-//   target: document.getElementById('app'),
-// })
+const app = new App({
+  target: document.getElementById('app'),
+});
 
-// export default app
+export default app;
